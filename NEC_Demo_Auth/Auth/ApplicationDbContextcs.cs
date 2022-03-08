20a69e7b4738840476c056a68f016b2fe2d08df0
attili.teja <attili.teja@BLR-LAP-108>
2022-03-08 11:01:54 +0000

    Add project files.

---
+++ NEC_Demo_Auth/Auth/ApplicationDbContextcs.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NEC_Demo_Auth.Auth
+{
+    public class ApplicationDbContextcs:IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContextcs(DbContextOptions<ApplicationDbContextcs> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+}
---
+++ NEC_Demo_Auth/UserrRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace NEC_Demo_Auth
+{
+    public  static class UserrRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
